ae99e96175a1e1578814d4404a57a2a686a41b8b
tmclean85 <trevor.a.mclean@gmail.com>
2017-06-04 22:41:24 -0700

    Add colonist ID to encounter reading pane

--- src/app/pages/encounters/encounters.component.ts
+++ src/app/pages/encounters/encounters.component.ts
@@ -10,25 +10,13 @@ import { EncountersService } from '../../services/encounters.service'
 })
 export class EncountersComponent implements OnInit {  
 
-    // interval; //Declared for use below
-    // message = "Hello";
-    // listOfMessages = [
-    //   "Hi",
-    //   "Brruuuhhhh",
-    //   "Word",
-    //   "Guten tag",
-    //   "Kiddah",
-    //   "Sod off m8",
-    //   "Morning!",
-    //   "Konichi wa",
-    // ];
+  
   encounters: Encounter[] = [];
 
 
   constructor(private encounterService: EncountersService) { 
     
   }
-// interval list populate demo below
   ngOnInit() {
     this.encounterService.getData()
     .subscribe((data) => {
@@ -36,17 +24,6 @@ export class EncountersComponent implements OnInit {
     });   
     }; 
 
-    
-
-    // this.interval = setInterval(() => {    
-    // let index = Math.floor(Math.random() * this.listOfMessages.length);
-    // this.message = this.listOfMessages[index];
-    // this.listOfMessages.push(this.message);
-    // }, 500);
   }
 
-// stopTheMadness(e) {
-//   e.preventDefault();
-//   clearInterval(this.interval);
-// }
 
